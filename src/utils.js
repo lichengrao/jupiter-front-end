364cfb46715ba8a6f206c8e2496dee034dda156d
lichengrao <lichengrao@gmail.com>
2021-04-04 15:21:09 -0400

    format folder

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,21 @@
 // If you don't want to host your server code and client code together, you can pay AWS to host your server with HTTPS then config the api url endpoints like below
 // const SERVER_ORIGIN = '<Your server's url>';
 
-const SERVER_ORIGIN = "";
+const SERVER_ORIGIN = '';
 
 const loginUrl = `${SERVER_ORIGIN}/login`;
 
 export const login = (credential) => {
   return fetch(loginUrl, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-    credentials: "include",
+    credentials: 'include',
     body: JSON.stringify(credential),
   }).then((response) => {
     if (response.status != 200) {
-      throw Error("Failed to log in");
+      throw Error('Failed to log in');
     }
 
     return response.json();
@@ -26,14 +26,14 @@ const registerUrl = `${SERVER_ORIGIN}/register`;
 
 export const register = (data) => {
   return fetch(registerUrl, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
   }).then((response) => {
     if (response.status !== 200) {
-      throw Error("Failed to register");
+      throw Error('Failed to register');
     }
   });
 };
@@ -42,11 +42,11 @@ const logoutUrl = `${SERVER_ORIGIN}/logout`;
 
 export const logout = () => {
   return fetch(logoutUrl, {
-    method: "POST",
-    credentials: "include",
+    method: 'POST',
+    credentials: 'include',
   }).then((response) => {
     if (response.status !== 200) {
-      throw Error("Failed to log out");
+      throw Error('Failed to log out');
     }
   });
 };
