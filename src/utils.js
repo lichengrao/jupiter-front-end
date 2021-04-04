f218e0a36baa4fd0a9563f6459ec4fcf88ee67c2
lichengrao <lichengrao@gmail.com>
2021-04-04 15:16:32 -0400

    initialize project

---
+++ src/utils.js
@@ -0,0 +1,52 @@
+// If you don't want to host your server code and client code together, you can pay AWS to host your server with HTTPS then config the api url endpoints like below
+// const SERVER_ORIGIN = '<Your server's url>';
+
+const SERVER_ORIGIN = "";
+
+const loginUrl = `${SERVER_ORIGIN}/login`;
+
+export const login = (credential) => {
+  return fetch(loginUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(credential),
+  }).then((response) => {
+    if (response.status != 200) {
+      throw Error("Failed to log in");
+    }
+
+    return response.json();
+  });
+};
+
+const registerUrl = `${SERVER_ORIGIN}/register`;
+
+export const register = (data) => {
+  return fetch(registerUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (response.status !== 200) {
+      throw Error("Failed to register");
+    }
+  });
+};
+
+const logoutUrl = `${SERVER_ORIGIN}/logout`;
+
+export const logout = () => {
+  return fetch(logoutUrl, {
+    method: "POST",
+    credentials: "include",
+  }).then((response) => {
+    if (response.status !== 200) {
+      throw Error("Failed to log out");
+    }
+  });
+};
